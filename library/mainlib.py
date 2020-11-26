6fe43fee43fbabedc31ad562ba9f9e48c02f2b81
LarsZauberer <wasser.ian@gmail.com>
2020-11-26 15:51:35 +0100

    Logging extended

--- library/mainlib.py
+++ library/mainlib.py
@@ -9,6 +9,8 @@ def drawWorld(data, x, y, bound, size):
     for i in range(len(data)):
         for j in range(len(data)):
             if x == i and y == j:
+                log.debug(f"Draw Player at Location {x} and {y}")
                 pygame.draw.rect(pygame.display.get_surface(), (255, 0, 0), (bound+x*size, bound+y*size))
             elif data[i][j] == 1:
+                log.debug(f"Draw Obstacle at Location {i} and {j}")
                 pygame.draw.rect(pygame.display.get_surface(), (96, 96, 96), (bound+x*size, bound+y*size))
